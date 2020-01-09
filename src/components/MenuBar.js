de656f810bb43d6c9d27469523f6e20f65f854c8
github username <harr.hughes@gmail.com>
2020-01-09 11:39:55 +0000

    Done.

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -2,32 +2,27 @@ import React from 'react'
 
 const MenuBar = (props) => {
 
-  /*
-
-  The 'a' tags below are the menu items. Think about the way a menu 
-  should work. When you click a menu item, the button typically becomes
-  'active' to indicate that it is currently selected. How could we achieve
-  this programatically? What other behavior do we expect when we click
-  on a menu item? Do we need state in this component, and if not, how can
-  this component be made aware of what is currently the active menu item?
-
-  */
-
+  
+  const calcClassName = id => {
+    return id === props.selectedTagId ? "item active" : "item"
+  }
   return (
+    
+    
     <div className="ui four item menu">
-      <a className="item active" id="profile">
+      <a className={calcClassName("profile")} id="profile" onClick={e => props.tagClick(e.target.id)}>
         <i className="user large icon" id="profile"/>
       </a>
 
-      <a className="item" id="photo">
+      <a className={calcClassName("photo")} id="photo" onClick={e => props.tagClick(e.target.id)}>
         <i className="photo large icon" id="photo"/>
       </a>
 
-      <a className="item" id="cocktail">
+      <a className={calcClassName("cocktail")} id="cocktail" onClick={e => props.tagClick(e.target.id)}>
         <i className="cocktail large icon" id="cocktail"/>
       </a>
 
-      <a className="item" id="pokemon"> 
+      <a className={calcClassName("pokemon")} id="pokemon" onClick={e => props.tagClick(e.target.id)}> 
         <i className=" themeisle large icon" id="pokemon"/>
       </a>
     </div>
--- src/containers/MainBox.js
+++ src/containers/MainBox.js
@@ -4,21 +4,26 @@ import { Profile, Photos, Cocktails, Pokemon} from '../components/Pages.js'
 
 class MainBox extends React.Component {
 
+  state = {
+    selectedTagId: "profile"
+  }
+
+  tagClick = id => {
+    this.setState({selectedTagId: id})
+  }
+
+  whichDetails = () => {
+    if (this.state.selectedTagId === "profile") return <Profile />;
+    else if (this.state.selectedTagId === "photo") return <Photos />;
+    else if (this.state.selectedTagId === "cocktail") return <Cocktails />;
+    else return <Pokemon />;
+  }
 
   render() {
-
-    /*
-
-    Replace the code below! Depending on what menu item is selected in the menu, I should render either a Profile, Photos, Cocktails, or Pokemon component.Think of a way to track which menu item was selected. Which component should have state? Which component should have methods to control state? Where should these methods be called?
-
-    */
-
-    const detailsToDisplay = <div>Hi, I'm a div!</div>
-
     return (
       <div>
-        <MenuBar />
-        {detailsToDisplay}
+        <MenuBar selectedTagId={this.state.selectedTagId} tagClick={id => this.tagClick(id)}/>
+        {this.whichDetails()}
       </div>
     )
   }
